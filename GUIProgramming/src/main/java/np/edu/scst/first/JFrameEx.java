be6af88da9578a319a44d19c20e0b1d6e4502e70
crpoudyal <crpoudyal@gmail.com>
2022-01-09 14:17:53 +0545

    GUIProgramming

---
+++ GUIProgramming/src/main/java/np/edu/scst/first/JFrameEx.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package np.edu.scst.first;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author crpoudyal
+ */
+public class JFrameEx {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Advance Java Programming");
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+
+    }
+}

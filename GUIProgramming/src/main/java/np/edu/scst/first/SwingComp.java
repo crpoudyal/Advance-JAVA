b722d58c7c7b0caca553184f1421fb23d64f1fbb
crpoudyal <crpoudyal@gmail.com>
2022-01-16 08:16:15 +0545

    more swing component

--- GUIProgramming/src/main/java/np/edu/scst/first/SwingComp.java
+++ GUIProgramming/src/main/java/np/edu/scst/first/SwingComp.java
@@ -7,17 +7,46 @@ public class SwingComp extends JFrame {
 
     SwingComp() {
 
-        JLabel lbl = new JLabel("Hello");
-        JLabel lbl1 = new JLabel("Hello world");
+        JLabel lbl = new JLabel("Enter");
 
         add(lbl);
+
+        JTextField txt = new JTextField("Enter Username", 20);
+
+        add(txt);
+        JLabel lbl2 = new JLabel("Password");
+        add(lbl2);
+
+        JPasswordField jpass = new JPasswordField(20);
+        add(jpass);
+
+        JLabel lbl1 = new JLabel("TextArea");
         add(lbl1);
+
+        JTextArea txtarea = new JTextArea(5, 20);
+        add(txtarea);
+
+        JLabel lbl3 = new JLabel("Gender");
+        add(lbl3);
+
+        JRadioButton male = new JRadioButton("Male");
+        JRadioButton female = new JRadioButton("Female");
+
+        ButtonGroup btngrp = new ButtonGroup();
+        add(male);
+        add(female);
+
+        btngrp.add(male);
+        btngrp.add(female);
+
+        JLabel lbl4 = new JLabel("Countries");
+        add(lbl4);
 
         setLayout(new FlowLayout());
 
         setVisible(true);
 
-        setSize(500, 500);
+        setSize(300, 500);
 
         setTitle("Swing Component");
 

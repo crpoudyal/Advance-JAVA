e51b54f8f2c407529784015d944e855e7e5d06ef
crpoudyal <crpoudyal@gmail.com>
2022-01-17 08:24:34 +0545

    More 2D shapes

--- GUIProgramming/src/main/java/np/edu/scst/first/DrawingShapes.java
+++ GUIProgramming/src/main/java/np/edu/scst/first/DrawingShapes.java
@@ -10,7 +10,23 @@ public class DrawingShapes extends JFrame {
             @Override
             public void paint(Graphics g) {
                 Graphics2D g2 = (Graphics2D) g;
-                g2.drawLine(10, 50, 50, 50);
+                //  line
+                g2.drawLine(10, 10, 40, 40);
+                //  rectangle
+                g2.drawRect(50, 10, 50, 30);
+                //  square
+                g2.drawRect(130, 80, 40, 40);
+                //  oval
+                g2.drawOval(10, 70, 50, 30);
+                //  circle
+                g2.drawOval(70, 90, 50, 50);
+                //  Arc
+                g2.drawArc(150, 150, 200, 200, 10, 80);
+                // Polygon
+                int[] x = {200, 500, 800, 700};
+                int[] y = {200, 500, 300, 600};
+                g2.drawPolygon(x, y, 4);
+
             }
 
         };

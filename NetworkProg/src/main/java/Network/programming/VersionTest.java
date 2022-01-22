103e98a66f9cac73e22378f3b36da30432f1249e
crpoudyal <crpoudyal@gmail.com>
2022-01-22 09:41:32 +0545

    version test ipv4 or ipv6

--- NetworkProg/src/main/java/Network/programming/VersionTest.java
+++ NetworkProg/src/main/java/Network/programming/VersionTest.java
@@ -1,5 +1,27 @@
 package Network.programming;
 
+import java.net.*;
+
 public class VersionTest {
+
+    public static int getVersion(InetAddress addr) {
+
+        byte[] address = addr.getAddress();
+
+        if (address.length == 4) {
+            return 4;
+        } else if (address.length == 16) {
+            return 6;
+        } else {
+            return -1;
+        }
+
+    }
+
+    public static void main(String[] args) throws UnknownHostException {
+
+        int res = getVersion(InetAddress.getByName("2400:1a00:b050:bd50:e5b2:5de:6d75:86cd"));
+        System.out.println(res);
+    }
 
 }

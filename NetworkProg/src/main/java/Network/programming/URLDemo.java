8a7b6d5737e1f685ed192dceb7383e1553a95bb8
crpoudyal <crpoudyal@gmail.com>
2022-01-26 09:06:37 +0545

    URL changes

--- NetworkProg/src/main/java/Network/programming/URLDemo.java
+++ NetworkProg/src/main/java/Network/programming/URLDemo.java
@@ -6,12 +6,13 @@ public class URLDemo {
 
     public static void main(String[] args) {
         try {
-            URL ul = new URL("http://www.example.com:80/index.html");
+            URL ul = new URL("http://www.example.com:80/java/index.html#xtocid12345");
             System.out.println("Protocol    : " + ul.getProtocol());
             System.out.println("File        : " + ul.getFile());
             System.out.println("Host        : " + ul.getHost());
             System.out.println("Port        : " + ul.getPort());
             System.out.println("Path        : " + ul.getPath());
+            System.out.println("Fragment Id : " + ul.getRef());
         } catch (Exception ex) {
             System.out.println("Exception" + ex);
         }

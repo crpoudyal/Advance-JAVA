c310f06b02db9df97c61fe9ff8342dd95c9c8caa
crpoudyal <crpoudyal@gmail.com>
2022-01-28 15:43:03 +0545

    assignment 1 completed

---
+++ GUIProgramming/src/main/java/np/edu/scst/assignment/EligibleToVote.java
@@ -0,0 +1,46 @@
+package np.edu.scst.assignment;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EligibleToVote extends JFrame {
+
+    EligibleToVote() {
+        JLabel lbl = new JLabel("Enter Your Age : ");
+        JTextField txt = new JTextField(3);
+        JLabel res = new JLabel();
+        JButton btn = new JButton("Check");
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String tx = txt.getText().toString();
+                int age = Integer.parseInt(tx);
+                if (age >= 18) {
+                    res.setText("Your age is " + age + "\nYou can vote in comming elections.");
+                } else {
+                    res.setText("Your age is " + age + "\nYou can't vote in comming elections.\nYour age must be greater and equals to 18");
+                }
+
+            }
+        });
+
+        add(lbl);
+        add(txt);
+        add(res);
+        add(btn);
+        setLayout(new FlowLayout());
+        setVisible(true);
+        setDefaultCloseOperation(3);
+        setTitle("Eligible to Vote");
+        setSize(300, 300);
+
+    }
+
+    public static void main(String[] args) {
+        new EligibleToVote();
+    }
+
+}

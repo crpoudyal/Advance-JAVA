1c6c65244f29567807d09b2e54af32bc5953b5d2
crpoudyal <crpoudyal@gmail.com>
2022-02-16 09:23:28 +0545

    drawing star shape in swing

---
+++ GUIProgramming/src/main/java/np/edu/scst/lab1/StarShape.java
@@ -0,0 +1,32 @@
+package np.edu.scst.lab1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StarShape extends JFrame {
+
+    StarShape() {
+        JPanel jpanel = new JPanel() {
+            @Override
+            public void paint(Graphics g) {
+                Graphics2D g2d = (Graphics2D) g;
+                int xPnts[] = {42, 52, 72, 52, 60, 40, 15, 28, 9, 32, 42};
+                int yPnts[] = {38, 62, 68, 80, 105, 85, 102, 75, 58, 60, 38};
+                g2d.drawPolygon(xPnts, yPnts, xPnts.length);
+            }
+
+        };
+        add(jpanel);
+        setVisible(true);
+        setTitle("STAR Shape in Swing");
+        setSize(250, 250);
+        setDefaultCloseOperation(3);
+
+    }
+
+    public static void main(String[] args) {
+        new StarShape();
+
+    }
+
+}

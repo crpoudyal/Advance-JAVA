56afd5e98470b1f1558a447d71d0ec1652b7f5cc
crpoudyal <crpoudyal@gmail.com>
2022-03-10 15:43:47 +0545

    socket info

---
+++ NetworkProg/src/main/java/socket/programming/SocketInfo.java
@@ -0,0 +1,20 @@
+package socket.programming;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class SocketInfo {
+
+    public static void main(String[] args) throws IOException, UnknownHostException, SocketException {
+        for (String host : args) {
+
+            Socket theSocket = new Socket(host, 80); // http == 80 , https == 443
+            System.out.println("connect to " + theSocket.getInetAddress() + " on port " + theSocket.getPort()
+                    + " from port" + theSocket.getLocalAddress() + " on port " + theSocket.getLocalPort());
+
+        }
+
+    }
+}
